b1c44466a95dd3247c477f37481765fea860e592
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-29 17:11:20 +0500

    Initial commit

---
+++ lib/pages/password_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PasswordPage extends StatefulWidget {
+  const PasswordPage({super.key});
+
+  @override
+  State<PasswordPage> createState() => _PasswordPageState();
+}
+
+class _PasswordPageState extends State<PasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/phone_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PhonePage extends StatefulWidget {
+  const PhonePage({super.key});
+
+  @override
+  State<PhonePage> createState() => _PhonePageState();
+}
+
+class _PhonePageState extends State<PhonePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
